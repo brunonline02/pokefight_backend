892590dbaeac996554e9325c42941c00ff6085e3
Bruno Silveira <bruno.silveira02@hotmail.com>
2023-02-27 18:03:30 +0100

    improve server

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 import express from "express";
+import bodyparser from "body-parser";
+import * as dotenv from "dotenv";
+import cors from "cors";
 import pokemonRouter from "./routes/pokemon.js";
+
+dotenv.config();
 
 const app = express();
 
-// Enable all CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 // Use the Pokemon router for all routes starting with /pokemon
 app.use("/pokemon", pokemonRouter);
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
 });
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import {
   getAllPokemon,
   getPokemonById,
-  getPokemonInfoById,
 } from "../controllers/pokemonController.js";
 
 const pokemonRouter = Router();
@@ -12,8 +11,5 @@ pokemonRouter.get("/", getAllPokemon);
 
 // Route to get only one pokemon from the JSON thanks to its id
 pokemonRouter.get("/:id", getPokemonById);
-
-// Route to get only one pokemon from the JSON thanks to its id and retrieve only one information (name or type or base) to send back to the client
-pokemonRouter.get("/:id/:info", getPokemonInfoById);
 
 export default pokemonRouter;
